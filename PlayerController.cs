90e1f8fe2330bd4a49d7981759954106a35744d2
furkansancu <furkansancu@outlook.com>
2023-06-07 04:00:31 +0300

    camera rotation added

--- PlayerController.cs
+++ PlayerController.cs
@@ -13,6 +13,11 @@ public class PlayerController : MonoBehaviour
     [Header("Controller Customizer")]
     public bool resetPropsOnStart = true;
 
+    [Header("Camera Settings")]
+    public Transform playerCamera;
+    public float mouseSensivity = 1.8f;
+    public float cameraMaxAngle = 89f;
+
     [Header("Player Properties")]
     public int health = 100;
     public float stamina = 100;
@@ -55,9 +60,15 @@ public class PlayerController : MonoBehaviour
             stamina = maxStamina;
             speed = moveSpeed;
         }
+
+        if (playerCamera != null) {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
     }
     
     void Update () {
+        if (playerCamera != null) CameraRotation();
         isGrounded = CheckGrounded();
         isMoving = rb.velocity.x != 0 || rb.velocity.z != 0;
         isSprinting = CharacterSprint();
@@ -74,7 +85,7 @@ public class PlayerController : MonoBehaviour
         Vector3 newVelocity = moveVelocity;
         if (newVelocity.y == 0) newVelocity.y = rb.velocity.y;
         newVelocity += FallMultiplier(newVelocity);
-        rb.velocity = newVelocity;
+        rb.velocity = transform.TransformDirection(newVelocity);
     }
 
     void CharacterMovement () {
@@ -90,6 +101,21 @@ public class PlayerController : MonoBehaviour
             return false;
 
         return isSprinting;
+    }
+
+    void CameraRotation () {
+        float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensivity;
+        float mouseY = -Input.GetAxisRaw("Mouse Y")  * mouseSensivity;
+
+        // Rotates camera for y axis
+        playerCamera.transform.Rotate(mouseY, 0, 0);
+        // Prevents camera from turning 360
+        float clampX = ClampAngle(playerCamera.localEulerAngles.x, -cameraMaxAngle, cameraMaxAngle);
+        playerCamera.localRotation = Quaternion.Euler(clampX, 0, 0);
+
+        // Rotates player for x axis
+        transform.Rotate(0, mouseX, 0);
+        transform.localRotation = Quaternion.Euler(0, transform.localEulerAngles.y, 0);
     }
 
     float UpdateStamina () {
@@ -108,7 +134,7 @@ public class PlayerController : MonoBehaviour
         return stamina;
     }
 
-    // !! Make sure to set player collider tag as "Player"
+    // !! Make sure to set player collider layer as "Player"
     bool CheckGrounded () {
         int layerMask =~ LayerMask.GetMask("Player");
         float sphereRadius = 0.25f;
@@ -123,7 +149,14 @@ public class PlayerController : MonoBehaviour
     // Makes fall behaviour more realistic.
     Vector3 FallMultiplier (Vector3 velocity) {
         if (velocity.y == 0 || Input.GetKeyDown(jumpKey)) return Vector3.zero;
-        float fallMultp = velocity.y < 0 ? 1.5f : 1f;
-        return Vector3.up * Physics.gravity.y * fallMultp * Time.deltaTime;
+        float multiplier = velocity.y < 0 ? 1.5f : 1f;
+        return Vector3.up * Physics.gravity.y * multiplier * Time.deltaTime;
+    }
+
+    // Clamps negative and positive angles
+    public static float ClampAngle (float angle, float min, float max) {
+        float start = (min + max) / 2 - 180;
+        float floor = Mathf.FloorToInt((angle - start) / 360) * 360;
+        return Mathf.Clamp(angle, min + floor, max + floor);
     }
 }

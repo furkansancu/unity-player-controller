ae06110ef1e60974c7f5dc91ae208c06f5b611c5
furkansancu <furkansancu@outlook.com>
2023-06-07 16:18:53 +0300

    Headbob added

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,8 +1,9 @@
 ﻿// https://github.com/furkansancu/unity-character-controller
 
 // GameObject Installion:
-// Player = +Rigidbody +CapsuleCollider +PlayerController
-// Player -> Layer = "Player"
+// ──> Player -> +Rigidbody +CapsuleCollider +PlayerController (Layer = "Player")
+//   └──> Camera Holder ->
+//      └──> Player Camera -> +Camera +AudioListener
 
 using System.Collections;
 using System.Collections.Generic;
@@ -12,21 +13,26 @@ public class PlayerController : MonoBehaviour
 {
     [Header("Controller Customizer")]
     public bool resetPropsOnStart = true;
+    public bool canSprint = true;
 
     [Header("Camera Settings")]
+    public bool headBob = true;
+    public Transform cameraHolder;
     public Transform playerCamera;
     public float mouseSensivity = 1.8f;
     public float cameraMaxAngle = 89f;
+    [Range(0.001f, 0.005f)] public float bobAmplitude = 0.0025f;
+    [Range(0, 25)] public float bobFrequency = 8.0f;
 
     [Header("Player Properties")]
     public int health = 100;
     public float stamina = 100;
-    public float speed = 5;
+    public float speed = 3;
 
     [Header("Player Settings")]
     public int maxHealth = 100;
     public float maxStamina = 100;
-    public float jumpSize = 3f;
+    public float jumpSize = 5f;
     public float moveSpeed = 3f;
     public float sprintSpeed = 5f;
     public float sprintDuration = 7f;
@@ -48,6 +54,8 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private CapsuleCollider capsuleCollider;
     private Vector3 moveVelocity = Vector3.zero;
+    private float bobToggleSpeed = 0.1f;
+    private Vector3 bobStartPosition;
 
     void OnValidate () {
         TryGetComponent<Rigidbody>(out rb);
@@ -65,16 +73,21 @@ public class PlayerController : MonoBehaviour
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
         }
+
+        if (headBob) {
+            bobStartPosition = playerCamera.transform.localPosition;
+        }
     }
     
     void Update () {
-        if (playerCamera != null) CameraRotation();
         isGrounded = CheckGrounded();
         isMoving = rb.velocity.x != 0 || rb.velocity.z != 0;
-        isSprinting = CharacterSprint();
+        isSprinting = canSprint && CharacterSprint();
         speed = isSprinting ? sprintSpeed : moveSpeed;
         stamina = UpdateStamina();
         CharacterMovement();
+        if (playerCamera != null && headBob) HeadBob();
+        if (playerCamera != null) CameraRotation();
 
         // Jump
         if (isGrounded && Input.GetKeyDown(jumpKey))
@@ -108,14 +121,59 @@ public class PlayerController : MonoBehaviour
         float mouseY = -Input.GetAxisRaw("Mouse Y")  * mouseSensivity;
 
         // Rotates camera for y axis
-        playerCamera.transform.Rotate(mouseY, 0, 0);
+        cameraHolder.transform.Rotate(mouseY, 0, 0);
         // Prevents camera from turning 360
-        float clampX = ClampAngle(playerCamera.localEulerAngles.x, -cameraMaxAngle, cameraMaxAngle);
-        playerCamera.localRotation = Quaternion.Euler(clampX, 0, 0);
+        float clampX = ClampAngle(cameraHolder.localEulerAngles.x, -cameraMaxAngle, cameraMaxAngle);
+        cameraHolder.localRotation = Quaternion.Euler(clampX, 0, 0);
 
         // Rotates player for x axis
         transform.Rotate(0, mouseX, 0);
         transform.localRotation = Quaternion.Euler(0, transform.localEulerAngles.y, 0);
+    }
+
+    void HeadBob () {
+        if (isGrounded) HeadShake();
+        if (!isGrounded) HeadFall();
+        ResetPosition();
+        CameraBobStabilize();
+    }
+
+    void HeadShake () {
+        float speed = new Vector3(rb.velocity.x, 0, rb.velocity.z).magnitude;
+        if (speed < bobToggleSpeed) return;
+        playerCamera.transform.localPosition += FootStepMotion();
+    }
+
+    void HeadFall () {
+        playerCamera.transform.localPosition += FallMotion(rb.velocity.y);
+    }
+
+    Vector3 FallMotion (float fallSpeed) {
+        fallSpeed = Mathf.Clamp(fallSpeed, -25, 25) * 2.5f;
+        Vector3 pos = Vector3.zero;
+        pos.y += -fallSpeed * Time.deltaTime;
+        return pos;
+    }
+
+    Vector3 FootStepMotion () {
+        Vector3 pos = Vector3.zero;
+        float netFrequency = (bobFrequency / moveSpeed) * speed;
+        float netAmplitude = (bobAmplitude / moveSpeed) * speed;
+        pos.y += Mathf.Sin(Time.time * netFrequency) * netAmplitude;
+        pos.x += Mathf.Cos(Time.time * netFrequency / 2) * netAmplitude * 2;
+        return pos;
+    }
+
+    void ResetPosition () {
+        if (playerCamera.transform.localPosition == bobStartPosition) return;
+        playerCamera.transform.localPosition = Vector3.Lerp(playerCamera.transform.localPosition, bobStartPosition, Time.deltaTime * 25f);
+    }
+
+    void CameraBobStabilize () {
+        Vector3 pos = playerCamera.transform.position;
+        pos.y += cameraHolder.localPosition.y;
+        pos += cameraHolder.transform.forward * 15f;
+        playerCamera.LookAt(pos);
     }
 
     float UpdateStamina () {
